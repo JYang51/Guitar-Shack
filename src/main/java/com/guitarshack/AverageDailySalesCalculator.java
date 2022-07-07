47b9c2b4c87b3b7f922510c05d80d0d8249bef3f
asavag19 <asavag19@ford.com>
2022-07-07 14:53:50 +0100

    Made AverageDailySalesCalculator use an end date of today.

--- src/main/java/com/guitarshack/AverageDailySalesCalculator.java
+++ src/main/java/com/guitarshack/AverageDailySalesCalculator.java
@@ -3,16 +3,18 @@ package com.guitarshack;
 import java.time.LocalDate;
 
 public class AverageDailySalesCalculator implements AverageDailySales {
+    private final LocalDate now;
     private SalesHistory salesHistory;
 
-    public AverageDailySalesCalculator(SalesHistory salesHistory) {
+    public AverageDailySalesCalculator(SalesHistory salesHistory, LocalDate now) {
 
         this.salesHistory = salesHistory;
 
+        this.now = now;
     }
 
     @Override
     public double ofProduct(Product product) {
-        return salesHistory.totalSales(product.getID(), LocalDate.now().minusDays(10), null)/10.0;
+        return salesHistory.totalSales(product.getID(), now.minusDays(10), now)/10.0;
     }
 }
--- src/test/java/com/guitarshack/AverageDailySalesTest.java
+++ src/test/java/com/guitarshack/AverageDailySalesTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class AverageDailySalesTest {
+    private final LocalDate now = LocalDate.of(2022, Month.JULY, 7);
     SalesHistory salesHistory;
 
     @BeforeEach
@@ -21,7 +22,7 @@ public class AverageDailySalesTest {
 
     @Test
     void averageCalculatedIsTotalSalesDividedByDays() {
-        AverageDailySales averageDailySales = new AverageDailySalesCalculator(salesHistory);
+        AverageDailySales averageDailySales = new AverageDailySalesCalculator(salesHistory, now);
         Product product = new Product(5, "agarg", 6, 4);
         double averageSales = averageDailySales.ofProduct(product);
         assertEquals(1.2, averageSales, 0);
@@ -29,8 +30,16 @@ public class AverageDailySalesTest {
 
     @Test
     void startDateUsedIs10DaysAgo() {
-        AverageDailySales averageDailySales = new AverageDailySalesCalculator(salesHistory);
+        AverageDailySales averageDailySales = new AverageDailySalesCalculator(salesHistory, now);
         averageDailySales.ofProduct(new Product(4, "", 4, 4));
         verify(salesHistory).totalSales(eq(4), eq(LocalDate.of(2022, Month.JUNE, 27)), any());
+    }
+
+    @Test
+    void endDateUsedIsToday() {
+        AverageDailySales averageDailySales = new AverageDailySalesCalculator(salesHistory, now);
+        averageDailySales.ofProduct(new Product(4, "", 4, 4));
+
+        verify(salesHistory).totalSales(anyInt(), any(), eq(now));
     }
 }
